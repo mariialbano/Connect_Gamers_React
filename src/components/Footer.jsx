79093ab1fa3dc091bd1609be3a526e6b405cd6a8
neemiasv <neemias.silva732017@gmail.com>
2025-09-02 22:09:54 -0300

    Ajustes no tema claro e escuro

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,6 @@
-import { useTheme } from '../theme/ThemeContext';
-
 export default function Footer() {
-  const { theme } = useTheme();
   return (
-    <footer className={
-      theme === "dark"
-        ? "bg-black text-gray-400 py-4 text-center mt-28 border-t border-pink-500"
-        : "bg-[#d1d2d6] text-gray-600 py-4 text-center mt-10 border-t border-pink-500"
-    }>
+    <footer className="bg-[#d1d2d6] dark:bg-black text-gray-600 dark:text-gray-400 py-4 text-center mt-28 dark:mt-28 border-t border-pink-500">
       <p className="text-sm">© {new Date().getFullYear()} Connect Gamers. Todos os direitos reservados.</p>
       <p className="text-sm">Desenvolvido por: Ana Gonçalves, Jessica Brito, Mariana Albano, Neemias Silva, Vinícius Gonzales.</p>
     </footer>
