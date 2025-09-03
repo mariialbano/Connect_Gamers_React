9c685c852a2aabee1f6fa66ee7c09698b7516272
neemiasv <neemias.silva732017@gmail.com>
2025-09-02 21:28:10 -0300

    Acessibilidade

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
-import { useTheme } from '../theme/ThemeContext';
 
 const FAQ = () => {
   const [rating, setRating] = useState(0);
   const [hoverRating, setHoverRating] = useState(0);
   const [feedback, setFeedback] = useState("");
-  const { theme } = useTheme();
 
   const handleRatingChange = setRating;
   const handleMouseOver = setHoverRating;
@@ -20,18 +18,14 @@ const FAQ = () => {
 
   return (
     <div className="my-24 flex items-center justify-center p-8">
-      <div className={
-        theme === "dark"
-          ? "w-full max-w-xl h-auto min-h-[420px] bg-gray-800/80 p-6 rounded-lg shadow-lg flex flex-col justify-between"
-          : "w-full max-w-xl h-auto min-h-[420px] bg-[#d9dbe2] p-6 rounded-lg shadow-lg flex flex-col justify-between"
-      }>
+      <div className="w-full max-w-xl h-auto min-h-[420px] bg-[#d9dbe2] dark:bg-gray-800/80 p-6 rounded-lg shadow-lg flex flex-col justify-between">
         <h2 className="text-3xl font-bold mb-4 text-center text-pink-500">
           Seu Feedback é importante para nós!
         </h2>
 
         <form onSubmit={handleFeedbackSubmit} className="flex flex-col flex-1 justify-between space-y-2">
           <div className="text-center">
-            <p className={`mb-2 text-lg ${theme === "dark" ? "text-white/80" : "text-black/90"}`}>
+            <p className="mb-2 text-lg text-black dark:text-white/80">
               Como você avalia sua experiência?
             </p>
             <div className="flex justify-center space-x-1 mb-3">
@@ -39,12 +33,10 @@ const FAQ = () => {
                 <button
                   type="button"
                   key={value}
-                  className={`text-2xl cursor-pointer  ${
+                  className={`text-2xl cursor-pointer ${
                     (hoverRating || rating) >= value
                       ? 'text-yellow-400'
-                      : theme === "dark"
-                        ? 'text-gray-300'
-                        : 'text-gray-600'
+                      : 'text-gray-600 dark:text-gray-300'
                   }`}
                   onClick={() => handleRatingChange(value)}
                   onMouseOver={() => handleMouseOver(value)}
@@ -57,11 +49,7 @@ const FAQ = () => {
           </div>
 
           <textarea 
-            className={`w-full p-2 border border-gray-300 rounded focus:border-gray-500/80 focus:ring-1 focus:ring-gray-500/80 outline-none ${
-              theme === "dark"
-                ? "bg-gray-200 text-black"
-                : "bg-gray-200 text-black"
-            }`}
+            className="w-full p-2 border border-gray-300 rounded focus:border-gray-500/80 focus:ring-1 focus:ring-gray-500/80 outline-none bg-gray-200 text-black dark:bg-gray-700 dark:text-white"
             placeholder="Deixe seu comentário..."
             value={feedback}
             onChange={(e) => setFeedback(e.target.value)}
